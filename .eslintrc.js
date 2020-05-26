b9dcf207f4c55d28598cd498d5a4a2d1346a24da
Karl Horky <karl.horky@gmail.com>
2020-05-26 15:25:30 +0200

    Add ESLint base config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'react-app',
+  rules: {
+    // Remove `href` warnings on anchor tags for Next.js
+    // Issue in Next.js: https://github.com/zeit/next.js/issues/5533
+    // Fix: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+  },
+};
