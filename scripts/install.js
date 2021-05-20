037a847c0199bd2eb8f536e9de87d520f0c33bfa
Karl Horky <karl.horky@gmail.com>
2021-05-20 17:59:33 +0200

    Change file extension

--- scripts/install.js
+++ scripts/install.js
@@ -8,7 +8,10 @@ packageJsonObj.resolutions = {
   'eslint-plugin-react': '7.23.2',
 };
 
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJsonObj, null, 2));
+fs.writeFileSync(
+  packageJsonPath,
+  JSON.stringify(packageJsonObj, null, 2) + '\n',
+);
 
 console.log(
   'Forced latest version of ESLint Plugin React with Yarn Resolution in package.json',
