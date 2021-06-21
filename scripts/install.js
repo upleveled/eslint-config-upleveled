069f62dc616780f7c85e8a14ce634d8ed54e3ffd
Karl Horky <karl.horky@gmail.com>
2021-06-21 11:33:15 +0200

    Add resolution for eslint-plugin-testing-library
    Fixes #56

--- scripts/install.js
+++ scripts/install.js
@@ -1,3 +1,4 @@
+import { execSync } from 'node:child_process';
 import fs from 'node:fs';
 
 const packageJsonPath = `${process.cwd()}/package.json`;
@@ -5,7 +6,17 @@ const packageJsonObj = JSON.parse(fs.readFileSync(packageJsonPath));
 
 packageJsonObj.resolutions = {
   ...packageJsonObj.resolutions,
+  // There is not yet a PR to upgrade this, but
+  // after facebook/create-react-app#10817 below
+  // is merged, it's possible that we can instead
+  // run `yarn upgrade react-scripts`, which acts
+  // the same as using this resolution (and then
+  // we don't need to keep maintaining this version
+  // number)
   'eslint-plugin-react': '7.24.0',
+  // Should be resolved by this PR:
+  // https://github.com/facebook/create-react-app/pull/10817
+  'eslint-plugin-testing-library': '4.6.0',
 };
 
 fs.writeFileSync(
@@ -16,3 +27,9 @@ fs.writeFileSync(
 console.log(
   'Forced latest version of ESLint Plugin React with Yarn Resolution in package.json',
 );
+
+console.log('Installing new packages from resolutions...');
+
+execSync('yarn install');
+
+console.log('Done');
