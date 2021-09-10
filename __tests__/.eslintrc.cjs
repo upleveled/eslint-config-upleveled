fa546d1dc4777527b14a3ed1128f09433d499fef
Karl Horky <karl.horky@gmail.com>
2021-09-10 18:39:09 +0200

    Upgrade tests

--- __tests__/.eslintrc.cjs
+++ __tests__/.eslintrc.cjs
@@ -1,9 +1,5 @@
 module.exports = {
   extends: ['@upleveled/upleveled'],
-  plugins: ['@upleveled/upleveled'],
-  rules: {
-    '@upleveled/upleveled/no-unnecessary-html-attributes': 'error',
-  },
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
