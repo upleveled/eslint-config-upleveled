9348617baceb7c766c7760a3709fa5fe54e9752c
José Fernando Höwer Barbosa <61600906+Josehower@users.noreply.github.com>
2022-01-26 18:50:14 +0100

    Add link to the docs and remove old rule

--- index.cjs
+++ index.cjs
@@ -156,10 +156,9 @@ module.exports = {
         aspects: ['invalidHref', 'preferButton'],
       },
     ],
-    // Warn about variable shadowing
-    // note you must disable the base rule as it can report incorrect errors
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error"]
+    // Warn about variable shadowing 
+    // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-shadow.md
+    '@typescript-eslint/no-shadow': 'error',
     // Warn on usage of var (which doesn't follow block scope rules)
     // https://eslint.org/docs/rules/no-var
     'no-var': 'warn',
