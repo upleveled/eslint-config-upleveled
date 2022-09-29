fdd9e6a3eab5105ccff204dd694d869657da5d92
Karl Horky <karl.horky@gmail.com>
2022-09-29 17:06:42 +0200

    Add SafeQL with config commented out

--- templates/.eslintrc.cjs
+++ templates/.eslintrc.cjs
@@ -1,3 +1,19 @@
 module.exports = {
   extends: ['@upleveled/upleveled'],
+  // plugins: ['@ts-safeql/eslint-plugin'],
+  // rules: {
+  //   '@ts-safeql/check-sql': [
+  //     'error',
+  //     {
+  //       connections: [
+  //         {
+  //           databaseUrl: 'postgres://postgres:postgres@localhost:5432/postgres',
+  //           tagName: 'sql',
+  //           fieldTransform: 'camel',
+  //           transform: '${type}[]',
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
 };
