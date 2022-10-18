aefee5d5e4c21dc866dce86806731d21e35dfdad
Karl Horky <karl.horky@gmail.com>
2022-10-18 18:35:07 +0200

    Conditionally enable SafeQL config
    If:
    
    1. Dependencies are available
    2. Environment variables are set
    3. If the platform is not Windows
    
    Closes #162

--- index.cjs
+++ index.cjs
@@ -1,5 +1,5 @@
 /** @type {import('eslint').ESLint.ConfigData} */
-const config = {
+let config = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
@@ -325,5 +325,51 @@ https://github.com/reactjs/reactjs.org/issues/4626#issuecomment-1117535930`,
     'unicorn/prefer-string-slice': 'warn',
   },
 };
+
+try {
+  if (process.platform === 'win32') {
+    throw new Error(
+      'SafeQL currently disabled on Windows https://github.com/ts-safeql/safeql/issues/80',
+    );
+  }
+
+  // Abort early if either of these modules are not installed
+  require.resolve('@ts-safeql/eslint-plugin');
+  require.resolve('dotenv-safe');
+
+  // @ts-ignore 2307 (module not found) -- The require.resolve() above will ensure that dotenv-safe is available before this line by throwing if it is not available
+  require('dotenv-safe').config();
+
+  if (
+    !process.env.PGHOST ||
+    !process.env.PGUSERNAME ||
+    !process.env.PGPASSWORD ||
+    !process.env.PGDATABASE
+  ) {
+    throw new Error('Environment variables are not set');
+  }
+
+  config = {
+    ...config,
+    plugins: ['@ts-safeql/eslint-plugin'],
+    rules: {
+      '@ts-safeql/check-sql': [
+        'error',
+        {
+          connections: [
+            {
+              databaseUrl: `postgres://${process.env.PGUSERNAME}:${process.env.PGPASSWORD}@${process.env.PGHOST}:5432/${process.env.PGDATABASE}`,
+              tagName: 'sql',
+              fieldTransform: 'camel',
+              transform: '{type}[]',
+            },
+          ],
+        },
+      ],
+    },
+  };
+} catch (err) {
+  // Swallow errors to avoid noisy ESLint logs in non-database projects
+}
 
 module.exports = config;
--- templates/.eslintrc.cjs
+++ templates/.eslintrc.cjs
@@ -1,19 +1,3 @@
 module.exports = {
   extends: ['@upleveled/upleveled'],
-  // plugins: ['@ts-safeql/eslint-plugin'],
-  // rules: {
-  //   '@ts-safeql/check-sql': [
-  //     'error',
-  //     {
-  //       connections: [
-  //         {
-  //           databaseUrl: 'postgres://postgres:postgres@localhost:5432/postgres',
-  //           tagName: 'sql',
-  //           fieldTransform: 'camel',
-  //           transform: '${type}[]',
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
 };
