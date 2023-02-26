823bcf453ce6e3121c5c857020169a259dc125d7
Karl Horky <karl.horky@gmail.com>
2023-02-26 19:29:39 +0100

    Disable Stylelint SCSS rules for CSS-in-JS
    See erroneous error reported by Stylelint with the postcss-styled-syntax syntax:
    
    https://github.com/hudochenkov/postcss-styled-syntax/issues/9

--- templates/stylelint.config.cjs
+++ templates/stylelint.config.cjs
@@ -1,16 +1,20 @@
 /** @type { import('stylelint').Config } */
 const config = {
-  extends: [
-    'stylelint-config-recommended',
-    'stylelint-config-recommended-scss',
-    'stylelint-config-css-modules',
-  ],
+  extends: ['stylelint-config-recommended'],
   rules: {
     'no-descending-specificity': null,
     // Allow files without any styles
     'no-empty-source': null,
   },
   overrides: [
+    {
+      files: ['**/*.css', '**/*.scss', '**/*.sass', '**/*.less'],
+      extends: ['stylelint-config-recommended-scss'],
+    },
+    {
+      files: ['**/*.module.css', '**/*.module.scss', '**/*.module.sass'],
+      extends: ['stylelint-config-css-modules'],
+    },
     {
       files: [
         '**/*.js',
