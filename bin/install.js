0789cfa8873176a675ccde4c569a4d7c9c5f04b2
Karl Horky <karl.horky@gmail.com>
2023-04-08 14:09:55 +0200

    Remove Next.js patch, add local next.d.ts file
    Ref: https://github.com/vercel/next.js/issues/45943#issuecomment-1500874994

--- bin/install.js
+++ bin/install.js
@@ -198,162 +198,162 @@ writeFileSync(
 
 console.log('✅ Done updating .gitignore');
 
-if ('next' in projectDependencies) {
-  const patchesPath = join(process.cwd(), 'patches');
+// if ('next' in projectDependencies) {
+//   const patchesPath = join(process.cwd(), 'patches');
 
-  // Remove previous patch files
-  if (existsSync(patchesPath)) {
-    const patchFiles = readdirSync(patchesPath);
+//   // Remove previous patch files
+//   if (existsSync(patchesPath)) {
+//     const patchFiles = readdirSync(patchesPath);
 
-    for (const patchFile of patchFiles) {
-      if (patchFile.startsWith('next@')) {
-        rmSync(join(patchesPath, patchFile));
-      }
-    }
-  }
+//     for (const patchFile of patchFiles) {
+//       if (patchFile.startsWith('next@')) {
+//         rmSync(join(patchesPath, patchFile));
+//       }
+//     }
+//   }
 
-  const nextVersion = JSON.parse(
-    execSync('pnpm list next --json', { encoding: 'utf-8' }),
-  )[0].dependencies.next.version;
+//   const nextVersion = JSON.parse(
+//     execSync('pnpm list next --json', { encoding: 'utf-8' }),
+//   )[0].dependencies.next.version;
 
-  const pnpmPatchNextEditDir = join(
-    process.cwd(),
-    'node_modules',
-    '.upleveled-next-patch',
-  );
+//   const pnpmPatchNextEditDir = join(
+//     process.cwd(),
+//     'node_modules',
+//     '.upleveled-next-patch',
+//   );
 
-  if (existsSync(pnpmPatchNextEditDir)) {
-    rmSync(pnpmPatchNextEditDir, { recursive: true });
-  }
+//   if (existsSync(pnpmPatchNextEditDir)) {
+//     rmSync(pnpmPatchNextEditDir, { recursive: true });
+//   }
 
-  execSync(
-    `pnpm patch next@${nextVersion} --edit-dir ${pnpmPatchNextEditDir}`,
-    {
-      // Discard stdout, show stderr
-      stdio: ['ignore', 'ignore', 'inherit'],
-    },
-  );
+//   execSync(
+//     `pnpm patch next@${nextVersion} --edit-dir ${pnpmPatchNextEditDir}`,
+//     {
+//       // Discard stdout, show stderr
+//       stdio: ['ignore', 'ignore', 'inherit'],
+//     },
+//   );
 
-  /**
-   * @typedef {{
-   *   lineNumber: number;
-   *   patternName: string;
-   *   pattern: RegExp;
-   *   replacement: string;
-   * }} Replacement
-   */
+//   /**
+//    * @typedef {{
+//    *   lineNumber: number;
+//    *   patternName: string;
+//    *   pattern: RegExp;
+//    *   replacement: string;
+//    * }} Replacement
+//    */
 
-  function replaceAll(
-    /** @type {string} */
-    filePath,
-    /** @type {string} */
-    content,
-    /** @type {Replacement[]} */
-    replacements,
-  ) {
-    for (const {
-      lineNumber,
-      patternName,
-      pattern,
-      replacement,
-    } of replacements) {
-      const match = content.match(pattern);
-      if (!match) {
-        throw new Error(`Pattern "${patternName}" not matched
+//   function replaceAll(
+//     /** @type {string} */
+//     filePath,
+//     /** @type {string} */
+//     content,
+//     /** @type {Replacement[]} */
+//     replacements,
+//   ) {
+//     for (const {
+//       lineNumber,
+//       patternName,
+//       pattern,
+//       replacement,
+//     } of replacements) {
+//       const match = content.match(pattern);
+//       if (!match) {
+//         throw new Error(`Pattern "${patternName}" not matched
 
-Regex: /${pattern.source}/m
-Source link: https://www.runpkg.com/?next@${nextVersion}/${filePath}#${lineNumber}`);
-      }
-      content = content.replace(pattern, replacement);
-    }
-    return content;
-  }
+// Regex: /${pattern.source}/m
+// Source link: https://www.runpkg.com/?next@${nextVersion}/${filePath}#${lineNumber}`);
+//       }
+//       content = content.replace(pattern, replacement);
+//     }
+//     return content;
+//   }
 
-  /**
-   * @type {{
-   *   filePath: string;
-   *   transform: (filePath: string, content: string) => string;
-   * }[]}
-   */
-  const transforms = [
-    // Apply diff:
-    // diff --git a/node_modules/next/dist/server/web/spec-extension/response.d.ts b/node_modules/next/dist/server/web/spec-extension/response.d.ts
-    // index 268f52b..6ef065b 100644
-    // --- a/node_modules/next/dist/server/web/spec-extension/response.d.ts
-    // +++ b/node_modules/next/dist/server/web/spec-extension/response.d.ts
-    // @@ -2,14 +2,15 @@ import type { I18NConfig } from '../../config-shared';
-    //  import { NextURL } from '../next-url';
-    //  import { ResponseCookies } from './cookies';
-    //  declare const INTERNALS: unique symbol;
-    // -export declare class NextResponse extends Response {
-    // +export declare class NextResponse<B = void> extends Response {
-    //      [INTERNALS]: {
-    //          cookies: ResponseCookies;
-    //          url?: NextURL;
-    // +        B: B
-    //      };
-    //      constructor(body?: BodyInit | null, init?: ResponseInit);
-    //      get cookies(): ResponseCookies;
-    // -    static json(body: any, init?: ResponseInit): NextResponse;
-    // +    static json<T>(body: T, init?: ResponseInit): NextResponse<T>;
-    //      static redirect(url: string | NextURL | URL, init?: number | ResponseInit): NextResponse;
-    //      static rewrite(destination: string | NextURL | URL, init?: MiddlewareResponseInit): NextResponse;
-    //      static next(init?: MiddlewareResponseInit): NextResponse;
-    {
-      filePath: join(
-        'dist',
-        'server',
-        'web',
-        'spec-extension',
-        'response.d.ts',
-      ),
-      transform: (filePath, content) => {
-        /** @type {Replacement[]} */
-        const replacements = [
-          {
-            lineNumber: 5,
-            patternName: 'export declare class NextResponse',
-            pattern:
-              /^(export declare class NextResponse)( extends Response \{\n)/m,
-            replacement: `$1<B = void>$2`,
-          },
-          {
-            lineNumber: 6,
-            patternName: 'NextResponse[INTERNALS]',
-            pattern:
-              /^( +\[INTERNALS\]: \{\n)( +)(cookies: ResponseCookies;\n)( +url\?: NextURL;\n)( +\};\n)/m,
-            replacement: `$1$2$3$4$2B: B;\n$5`,
-          },
-          {
-            lineNumber: 12,
-            patternName: 'NextResponse.json()',
-            pattern:
-              /^( +static json)(\(body: )any(, init\?: ResponseInit\): NextResponse)(;)/m,
-            replacement: `$1<T>$2T$3<T>$4`,
-          },
-        ];
+//   /**
+//    * @type {{
+//    *   filePath: string;
+//    *   transform: (filePath: string, content: string) => string;
+//    * }[]}
+//    */
+//   const transforms = [
+//     // Apply diff:
+//     // diff --git a/node_modules/next/dist/server/web/spec-extension/response.d.ts b/node_modules/next/dist/server/web/spec-extension/response.d.ts
+//     // index 268f52b..6ef065b 100644
+//     // --- a/node_modules/next/dist/server/web/spec-extension/response.d.ts
+//     // +++ b/node_modules/next/dist/server/web/spec-extension/response.d.ts
+//     // @@ -2,14 +2,15 @@ import type { I18NConfig } from '../../config-shared';
+//     //  import { NextURL } from '../next-url';
+//     //  import { ResponseCookies } from './cookies';
+//     //  declare const INTERNALS: unique symbol;
+//     // -export declare class NextResponse extends Response {
+//     // +export declare class NextResponse<B = void> extends Response {
+//     //      [INTERNALS]: {
+//     //          cookies: ResponseCookies;
+//     //          url?: NextURL;
+//     // +        B: B
+//     //      };
+//     //      constructor(body?: BodyInit | null, init?: ResponseInit);
+//     //      get cookies(): ResponseCookies;
+//     // -    static json(body: any, init?: ResponseInit): NextResponse;
+//     // +    static json<T>(body: T, init?: ResponseInit): NextResponse<T>;
+//     //      static redirect(url: string | NextURL | URL, init?: number | ResponseInit): NextResponse;
+//     //      static rewrite(destination: string | NextURL | URL, init?: MiddlewareResponseInit): NextResponse;
+//     //      static next(init?: MiddlewareResponseInit): NextResponse;
+//     {
+//       filePath: join(
+//         'dist',
+//         'server',
+//         'web',
+//         'spec-extension',
+//         'response.d.ts',
+//       ),
+//       transform: (filePath, content) => {
+//         /** @type {Replacement[]} */
+//         const replacements = [
+//           {
+//             lineNumber: 5,
+//             patternName: 'export declare class NextResponse',
+//             pattern:
+//               /^(export declare class NextResponse)( extends Response \{\n)/m,
+//             replacement: `$1<B = void>$2`,
+//           },
+//           {
+//             lineNumber: 6,
+//             patternName: 'NextResponse[INTERNALS]',
+//             pattern:
+//               /^( +\[INTERNALS\]: \{\n)( +)(cookies: ResponseCookies;\n)( +url\?: NextURL;\n)( +\};\n)/m,
+//             replacement: `$1$2$3$4$2B: B;\n$5`,
+//           },
+//           {
+//             lineNumber: 12,
+//             patternName: 'NextResponse.json()',
+//             pattern:
+//               /^( +static json)(\(body: )any(, init\?: ResponseInit\): NextResponse)(;)/m,
+//             replacement: `$1<T>$2T$3<T>$4`,
+//           },
+//         ];
 
-        return replaceAll(filePath, content, replacements);
-      },
-    },
-  ];
+//         return replaceAll(filePath, content, replacements);
+//       },
+//     },
+//   ];
 
-  for (const { filePath: relativeFilePath, transform } of transforms) {
-    const filePath = join(pnpmPatchNextEditDir, relativeFilePath);
-    console.log(`Patching node_modules/next/${relativeFilePath}...`);
-    writeFileSync(
-      filePath,
-      transform(relativeFilePath, readFileSync(filePath, 'utf8')),
-    );
-  }
+//   for (const { filePath: relativeFilePath, transform } of transforms) {
+//     const filePath = join(pnpmPatchNextEditDir, relativeFilePath);
+//     console.log(`Patching node_modules/next/${relativeFilePath}...`);
+//     writeFileSync(
+//       filePath,
+//       transform(relativeFilePath, readFileSync(filePath, 'utf8')),
+//     );
+//   }
 
-  console.log('Generating patch...');
+//   console.log('Generating patch...');
 
-  execSync(`pnpm patch-commit ${pnpmPatchNextEditDir}`, {
-    // Show stdout stderr
-    stdio: ['ignore', 'inherit', 'inherit'],
-  });
+//   execSync(`pnpm patch-commit ${pnpmPatchNextEditDir}`, {
+//     // Show stdout stderr
+//     stdio: ['ignore', 'inherit', 'inherit'],
+//   });
 
-  rmSync(pnpmPatchNextEditDir, { recursive: true });
-  console.log('✅ Done patching Next.js');
-}
+//   rmSync(pnpmPatchNextEditDir, { recursive: true });
+//   console.log('✅ Done patching Next.js');
+// }
