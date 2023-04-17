735966cc42860d07884cab8f4146c1588bf2aa97
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-17 18:36:39 +0200

    Use stylelint-config-upleveled standalone package (#220)
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- templates/stylelint.config.cjs
+++ templates/stylelint.config.cjs
@@ -1,40 +1,6 @@
 /** @type { import('stylelint').Config } */
 const config = {
-  extends: ['stylelint-config-recommended'],
-  rules: {
-    'no-descending-specificity': null,
-    // Allow files without any styles
-    'no-empty-source': null,
-  },
-  overrides: [
-    {
-      files: ['**/*.css', '**/*.scss', '**/*.sass', '**/*.less'],
-      extends: ['stylelint-config-recommended-scss'],
-      rules: {
-        'scss/at-rule-no-unknown': [
-          true,
-          {
-            ignoreAtRules: ['tailwind'],
-          },
-        ],
-      },
-    },
-    {
-      files: ['**/*.module.css', '**/*.module.scss', '**/*.module.sass'],
-      extends: ['stylelint-config-css-modules'],
-    },
-    {
-      files: [
-        '**/*.js',
-        '**/*.cjs',
-        '**/*.mjs',
-        '**/*.jsx',
-        '**/*.ts',
-        '**/*.tsx',
-      ],
-      customSyntax: 'postcss-styled-syntax',
-    },
-  ],
+  extends: ['stylelint-config-upleveled'],
 };
 
 module.exports = config;
