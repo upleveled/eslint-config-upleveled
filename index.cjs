d0e8ecb6e0c8aae4aee21f1d50e5a41d48055cc8
Karl Horky <karl.horky@gmail.com>
2023-05-05 19:15:40 +0200

    Specify ESLint config as root config

--- index.cjs
+++ index.cjs
@@ -372,6 +372,7 @@ const eslintConfigReactAppRules = {
 
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
