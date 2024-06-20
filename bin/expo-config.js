795fe65f33ae433eca2f3a75220b030a5dfed2d9
Lukas Prochazka <prochazka.lu@gmail.com>
2024-06-20 18:47:19 +0200

    Change comments

--- bin/expo-config.js
+++ bin/expo-config.js
@@ -1,19 +1,15 @@
 // Enable Expo non-default options for performance:
 //
-// 1. .env.development, .env.production - Enable the new Metro resolver available starting in Expo SDK 51
-//    https://github.com/EvanBacon/pillar-valley/commit/ede321ef7addc67e4047624aedb3e92af3cb5060
-// 2. app.json - Enable New Architecture for iOS and Android
+
+// 1. app.json - Enable New Architecture for iOS and Android
 //    https://docs.expo.dev/guides/new-architecture/
+// 2. .env.development, .env.production - Enable the new Metro resolver available starting in Expo SDK 51
+//    https://github.com/EvanBacon/pillar-valley/commit/ede321ef7addc67e4047624aedb3e92af3cb5060
 // 3. eas.json - Enable the new Metro resolver available starting in Expo SDK 51
 //    https://archive.ph/MG03E
 //
 // TODO: Remove when Expo enables New Architecture and new Metro resolver by default
 import { readFile, writeFile } from 'node:fs/promises';
-
-await writeFile('.env.development', 'EXPO_USE_FAST_RESOLVER=1', 'utf8');
-console.log('✅ Enabled New Metro Resolver in .env.development');
-await writeFile('.env.production', 'EXPO_USE_FAST_RESOLVER=1', 'utf8');
-console.log('✅ Enabled New Metro Resolver in .env.production');
 
 const appFilePath = 'app.json';
 const appJson = JSON.parse(await readFile(appFilePath, 'utf8'));
@@ -35,6 +31,11 @@ appJson.expo.plugins = [
 await writeFile(appFilePath, JSON.stringify(appJson, null, 2), 'utf8');
 console.log('✅ Enabled New Architecture in app.json');
 
+await writeFile('.env.development', 'EXPO_USE_FAST_RESOLVER=1', 'utf8');
+console.log('✅ Enabled new Metro resolver in .env.development');
+await writeFile('.env.production', 'EXPO_USE_FAST_RESOLVER=1', 'utf8');
+console.log('✅ Enabled new Metro resolver in .env.production');
+
 const easFilePath = 'eas.json';
 const easJson = JSON.parse(await readFile(easFilePath, 'utf8'));
 
@@ -54,4 +55,4 @@ easJson.build.preview.extends = 'base';
 easJson.build.production.extends = 'base';
 
 await writeFile(easFilePath, JSON.stringify(easJson, null, 2), 'utf8');
-console.log('✅ Extended build configuration for production environments');
+console.log('✅ Enabled new Metro resolver in eas.json');
