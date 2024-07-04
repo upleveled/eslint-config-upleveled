fed027c6d9925dbea237e98d3f5c41e7e9b38e26
Lukas Prochazka <prochazka.lu@gmail.com>
2024-07-04 15:23:07 +0200

    Add expo-postgresql template

---
+++ templates/expo-postgresql/prettier.config.js
@@ -0,0 +1,35 @@
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  plugins: ['prettier-plugin-embed', 'prettier-plugin-sql'],
+  singleQuote: true,
+  trailingComma: 'all',
+};
+
+/** @type {import('prettier-plugin-embed').PrettierPluginEmbedOptions} */
+const prettierPluginEmbedConfig = {
+  embeddedSqlComments: ['sql'],
+  embeddedSqlTags: ['sql'],
+};
+
+/** @type {import('prettier-plugin-sql').SqlBaseOptions} */
+const prettierPluginSqlConfig = {
+  language: 'postgresql',
+  keywordCase: 'upper',
+  identifierCase: 'lower',
+  dataTypeCase: 'lower',
+  functionCase: 'lower',
+  // - Wrap all parenthesized expressions to new lines (eg. `INSERT` columns)
+  // - Do not wrap foreign keys (eg. `REFERENCES table_name (id)`)
+  // - Do not wrap column type expressions (eg. `VARCHAR(255)`)
+  // - Do not wrap longer field names when used alone (eg. `address_line_one`)
+  // - Do not wrap shorter expressions in function calls (eg. `avg(ratings.rating)`)
+  expressionWidth: 30,
+};
+
+const config = {
+  ...prettierConfig,
+  ...prettierPluginEmbedConfig,
+  ...prettierPluginSqlConfig,
+};
+
+export default config;
