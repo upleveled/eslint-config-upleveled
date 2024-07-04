2fe818213c6740dffd2951fde643f4b50d952ef8
Karl Horky <karl.horky@gmail.com>
2024-07-04 14:51:40 +0200

    Allow async database query functions without cache() wrapper (#396)

--- index.js
+++ index.js
@@ -953,11 +953,28 @@ const getArticleCategoriesInsecure = cache(async () =>
 
 `,
         },
+        {
+          selector:
+            "ExportNamedDeclaration > VariableDeclaration[declarations.0.init.type='ArrowFunctionExpression'][declarations.0.id.name!=/Insecure$/][declarations.0.init.params.0.name!='sessionToken']",
+          message: `Ambiguous authentication of exported database query function - either pass \`sessionToken\` as the first parameter or destructured property in first parameter or name the function ending with \`Insecure\`:
+
+const getUser = async (sessionToken: string, userId: number) =>
+
+const getArticleCategoriesInsecure = async () =>
+
+`,
+        },
         // Enforce usage of session token ("sessionToken" first
         // parameter) within database functions
         {
           selector:
             "ExportNamedDeclaration > VariableDeclaration[declarations.0.init.callee.name='cache'][declarations.0.init.arguments.0.params.0.name='sessionToken'] > VariableDeclarator > CallExpression > ArrowFunctionExpression > BlockStatement:not(:has([type='Identifier'][name='sessionToken']))",
+          message:
+            'Unused `sessionToken` parameter in database query function - use `sessionToken` in database queries to implement authentication and authorization',
+        },
+        {
+          selector:
+            "ExportNamedDeclaration > VariableDeclaration[declarations.0.init.type='ArrowFunctionExpression'][declarations.0.init.params.0.name='sessionToken'] > VariableDeclarator > ArrowFunctionExpression > BlockStatement:not(:has([type='Identifier'][name='sessionToken']))",
           message:
             'Unused `sessionToken` parameter in database query function - use `sessionToken` in database queries to implement authentication and authorization',
         },
