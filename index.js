004b2c1368f2706c41aa6ef998a42aa5610a4197
Karl Horky <karl.horky@gmail.com>
2024-08-30 12:22:12 +0200

    Add comment with TODO

--- index.js
+++ index.js
@@ -514,11 +514,18 @@ const configArray = [
       },
       'jsx-a11y': jsxA11y,
       'jsx-expressions': jsxExpressions,
-      'react-hooks': fixupPluginRules(
-        /** @type {import('@eslint/compat').FixupPluginDefinition} */ (
-          reactHooks
+      'react-hooks':
+        // Fix eslint-plugin-react-hooks for ESLint 9
+        //
+        // TODO: Remove this once eslint-plugin-react-hooks fully
+        // supports ESLint 9
+        // - https://github.com/facebook/react/pull/30774
+        // - https://github.com/facebook/react/issues/28313#issuecomment-2069701429
+        fixupPluginRules(
+          /** @type {import('@eslint/compat').FixupPluginDefinition} */ (
+            reactHooks
+          ),
         ),
-      ),
       'import-x': eslintImportX,
       react: fixupPluginRules(react),
       security,
