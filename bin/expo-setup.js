ca81b513d606b34e4a98d92cf4c7b615b3342fd2
Lukas Prochazka <prochazka.lu@gmail.com>
2025-01-24 11:56:27 +0100

    Move import to code usage

--- bin/expo-setup.js
+++ bin/expo-setup.js
@@ -15,14 +15,6 @@ import { readFile, unlink, writeFile } from 'node:fs/promises';
 import { promisify } from 'node:util';
 import isPlainObject from 'is-plain-obj';
 
-// Install Prettier to format `app.config.ts`, colocated in this script for
-// easier removal
-//
-// TODO: Remove if `create-expo-app` creates `app.config.ts` in future:
-// - https://github.com/expo/expo/issues/34357
-await promisify(exec)('pnpm add --save-dev prettier');
-const { format } = await import('prettier');
-
 const appJsonFilePath = 'app.json';
 const appJson = JSON.parse(await readFile(appJsonFilePath, 'utf8'));
 
@@ -31,6 +23,14 @@ if (!isPlainObject(appJson) || !isPlainObject(appJson.expo)) {
     'app.json either contains non-object or contains object without .expo property',
   );
 }
+
+// Install Prettier to format `app.config.ts`, colocated in this script for
+// easier removal
+//
+// TODO: Remove if `create-expo-app` creates `app.config.ts` in future:
+// - https://github.com/expo/expo/issues/34357
+await promisify(exec)('pnpm add --save-dev prettier');
+const { format } = await import('prettier');
 
 await writeFile(
   'app.config.ts',
