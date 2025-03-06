9090a722dfc2aab70f50d3ebb182aeecd6a68896
Karl Horky <karl.horky@gmail.com>
2025-03-06 15:20:55 +0100

    Remove obsolete types

--- types/eslint-plugin-jsx-expressions.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'eslint-plugin-jsx-expressions' {
-  import type { FlatConfig } from '@typescript-eslint/utils/ts-eslint';
-
-  let plugin: FlatConfig.Plugin;
-  export default plugin;
-}
--- types/eslint-plugin-react.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'eslint-plugin-react' {
-  import type { ESLint } from 'eslint';
-
-  let plugin: ESLint.Plugin;
-  export default plugin;
-}
