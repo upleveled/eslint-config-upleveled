b233beb4252fd7bd3e8b1849c6f7f9f5a94476de
Karl Horky <karl.horky@gmail.com>
2026-09-15 21:10:49 +0200

    Clarify which npm publish the wait script waits for

--- scripts/waitForNpmPublish.ts
+++ scripts/waitForNpmPublish.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// Wait for npm publish, delayed by malware scanning
+// Wait for npm publish of package.json version, delayed by malware scanning
 // - https://github.blog/changelog/2026-07-28-npm-publish-time-malware-scanning-and-dual-use-metadata/
 
 import { readFileSync } from 'node:fs';
